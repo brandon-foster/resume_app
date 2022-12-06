ea110e2cc4b1daa9857d8f00b03458de0e7a9b96
brandon <brandon@brandons-MacBook-Pro.local>
2022-12-05 19:36:09 -0500

    cleaner resume items

--- apache/html/js/resume.js
+++ apache/html/js/resume.js
@@ -3,24 +3,34 @@ const resItems = [
         heading: 'CGI Federal',
         lines: [
             {
+                title: 'OpenShift to EKS Migration',
                 text: 'Transition microservices to EKS from OpenShift for development, UAT, and prod environments',                
                 tags: [
                     'EKS',
                 ],
             },
             {
+                title: 'Remove Technical Debt',
                 text: 'Refactor inherited code',
                 tags: [
                     'JUnit', 'Mockito', 'JaCoCo', 'Spring Boot', 'unit testing', 'Spring integration testing', 'controller testing', 'Java',
                 ],
             },
             {
+                title: 'Prod Support Sub Team',
                 text: 'Selected to join production support team to deliver priority enahancements and defect resolution',
                 tags: ['Java', 'PostgreSQL', 'Oracle', 'Bash', 'EKS'],
             },
         ],
     },
 ];
+
+function createTitle(title) {
+    const h3 = document.createElement('h3');
+    h3.innerHTML = title;
+    h3.classList.add('fw-light');
+    return h3;
+}
 
 function createP(text) {
     const p = document.createElement('p');
@@ -49,30 +59,36 @@ function createTagList(tagArr) {
 
 function createLineElem(lineObj) {
     const wrapper = document.createElement('div');
-    wrapper.classList.add('row', 'mt-4');
+    wrapper.classList.add('row', 'mt-5');
 
-    const colLeft = document.createElement('div');
-    colLeft.classList.add('col-md-3');
-    colLeft.append(createTagList(lineObj.tags));
+//    const colLeft = document.createElement('div');
+//    colLeft.classList.add('col-md-3');
+//    colLeft.append(createTagList(lineObj.tags));
+//
+//    const colRight = document.createElement('div');
+//    colRight.classList.add('col-md-9');
+//    colRight.append(createP(lineObj.text));
 
-    const colRight = document.createElement('div');
-    colRight.classList.add('col-md-9');
-    colRight.append(createP(lineObj.text));
-
-    wrapper.append(colLeft);
-    wrapper.append(colRight);
+    wrapper.append(createTitle(lineObj.title));
+//    wrapper.append(colLeft);
+//    wrapper.append(colRight);
+    wrapper.append(createP(lineObj.text));
+    wrapper.append(createTagList(lineObj.tags));
     return wrapper;
 }
 
-function makeResItemElem(resItem) {
-    const heading = document.createElement('h1');
-    heading.innerHTML = resItem.heading;
-    heading.classList.add('mt-5');
-    
-    const lineElems = resItem.lines.map(l => createLineElem(l));
+function createHeading(heading) {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = heading;
+    h2.classList.add('mt-5');
+    return h2;
+}
 
+function makeResItemElem(resItem) {
+    const lineElems = resItem.lines.map(l => createLineElem(l));
     const wrapper = document.createElement('div');
-    wrapper.append(heading);
+    wrapper.classList.add('w-max-42em', 'd-flex', 'w-100', 'text-center', 'mx-auto', 'mb-auto', 'flex-column');
+    wrapper.append(createHeading(resItem.heading));
     lineElems.forEach(le => wrapper.append(le));
     return wrapper;
 }
