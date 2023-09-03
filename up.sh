f2c3af9b963c1abee6f36151d82bb22977ed55f2
root <root@localhost>
2023-09-03 22:19:35 +0000

    up.sh set permission mode on executable files

--- up.sh
+++ up.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd ./puzzlesolver-api-spring-boot \
+
+chmod u+x down.sh \
+        puzzlesolver-api-spring-boot/build.sh \
+        puzzlesolver-api-spring-boot/mvnw \
+        url_app/url_service/build.sh \
+        url_app/url_service/mvnw \
+    && cd ./puzzlesolver-api-spring-boot \
     && ./build.sh \
     && cd - \
     && cd ./url_app/url_service \
